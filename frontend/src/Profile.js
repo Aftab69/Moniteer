6a96d7adf724acd0ed93ae6ff56deeb18dd13667
Aftab69 <aftabhssn3@gmail.com>
2023-03-22 18:33:49 +0530

    toggle buttons added

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -6,6 +6,8 @@ const Profile = () => {
   const navigate = useNavigate();
   const [ name, setName ] = useState("");
   const [ visibility, setVisibility ] = useState({display:"none"});
+  const [ togglebutton1, setTogglebutton1 ] = useState({background:"white"});
+  const [ togglebutton2, setTogglebutton2 ] = useState({background:"white"});
   const getData = async() =>{
       try{
         const res = await fetch("/profile",{
@@ -30,14 +32,28 @@ const Profile = () => {
     getData()
   },[]) // eslint-disable-line react-hooks/exhaustive-deps
 
+  const handleToggle1 = () =>{
+    setTogglebutton1({background:"grey"})
+    setTogglebutton2({background:"white"})
+  }
+  const handleToggle2 = () =>{
+    setTogglebutton2({background:"green"})
+    setTogglebutton1({background:"white"})
+  }
+
   return (
     <>
     <div className='profileMainContainer'>
       <div style={visibility}>
         <h1>Welcome, {name}</h1>
       </div>
-      <div className='toggleContainer'>
-
+      <div className='toggleMainContainer'>
+        <div onClick={handleToggle1} className='toggleContainer1' style={togglebutton1}>
+          <p>I'm Offline</p>
+        </div>
+        <div onClick={handleToggle2} className='toggleContainer2' style={togglebutton2}>
+          <p>I'm Online</p>
+        </div>
       </div>
     </div>
     </>
