ad89dc5a309705cbb465ac9ee88c69939d931f5e
Aftab69 <aftabhssn3@gmail.com>
2023-03-22 23:05:44 +0530

    toggle buttons color fix

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -22,6 +22,13 @@ const Profile = () => {
             const data = await res.json();
             setName(data.name)
             setEmail(data.email)
+            if(data.onlinestatus==="on"){
+              document.getElementById("toggleContainer2").style.background = "green";
+              document.getElementById("toggleContainer1").style.background = "white";
+            } else if(data.onlinestatus==="off"){
+              document.getElementById("toggleContainer1").style.background = "grey";
+              document.getElementById("toggleContainer2").style.background = "white";
+            }
             setVisibility({display:"block"})
         } else if(res.status===400) {
             navigate("/login")
@@ -34,9 +41,11 @@ const Profile = () => {
     getData()
   },[]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  const handleToggle1 = () =>{
+  const handleToggle1 = (e) =>{
+    e.preventDefault();
     setTogglebutton1({background:"grey"})
     setTogglebutton2({background:"white"})
+    document.getElementById("toggleContainer2").style.background = "white";
 
     fetch("/online",{
       method:"POST",
@@ -48,16 +57,19 @@ const Profile = () => {
       })
     }).then((res)=>{
       if(res.status===200){
-        alert("User offline")
+        // alert("User offline")
       } else {
         alert("error")
       }
     })
 
   }
-  const handleToggle2 = () =>{
+  
+  const handleToggle2 = (e) =>{
+    e.preventDefault();
     setTogglebutton2({background:"green"})
     setTogglebutton1({background:"white"})
+    document.getElementById("toggleContainer1").style.background = "white";
 
     fetch("/online",{
       method:"POST",
@@ -69,7 +81,7 @@ const Profile = () => {
       })
     }).then((res)=>{
       if(res.status===200){
-        alert("User online")
+        // alert("User online")
       } else {
         alert("error")
       }
@@ -84,10 +96,10 @@ const Profile = () => {
         <h1>Welcome, {name}</h1>
       </div>
       <div className='toggleMainContainer'>
-        <div onClick={handleToggle1} className='toggleContainer1' style={togglebutton1}>
+        <div onClick={handleToggle1} className='toggleContainer1' id='toggleContainer1' style={togglebutton1}>
           <p>I'm Offline</p>
         </div>
-        <div onClick={handleToggle2} className='toggleContainer2' style={togglebutton2}>
+        <div onClick={handleToggle2} className='toggleContainer2' id='toggleContainer2' style={togglebutton2}>
           <p>I'm Online</p>
         </div>
       </div>
