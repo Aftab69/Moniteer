9a0c1a79c3c8d26005085fdfa6592e2c53c71124
Sentinelhz <sentinelhz344@gmail.com>
2023-04-26 18:52:16 +0530

    cookie save

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -34,35 +34,35 @@ const Login = () => {
     })
       .then((res) => {
         if (res.status === 200) {
-          res.headers.forEach((value, name) => {
-            if (name === 'set-cookie') {
-              const jwtToken = value.split(';')[0].split('=')[1];
-              document.cookie = `jwt=${jwtToken}; path=/; expires=${new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toUTCString()};`;
-              alert("User successfully logged in")
-              navigate("/")
-              
-            } else if (res.status === 400) {
-              alert('Please fill in your form');
-            } else if (res.status === 401) {
-              alert('Invalid credentials');
-            }
-          })
-            .catch((error) => {
-              alert(`Error: ${error.message}`);
-            });
-        };
 
-        return (
-          <div className="loginpageContainer">
-            <form method="POST" onSubmit={handleSubmit} className="logininfoContainer">
-              <p>Email:</p>
-              <input type="email" name="email" onChange={handleChange} />
-              <p>Password:</p>
-              <input type="password" name="password" onChange={handleChange} />
-              <button type="submit">Login</button>
-            </form>
-          </div>
-        );
-      };
+          const jwtToken = res.headers.get("set-cookie");
+          document.cookie = `jwt=${jwtToken}; path=/; expires=${new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toUTCString()};`;
+          alert("User successfullxy logged in")
+          navigate("/")
 
-    export default Login;
+        } else if (res.status === 400) {
+          alert('Please fill in your form');
+        } else if (res.status === 401) {
+          alert('Invalid credentials');
+        }
+      })
+      .catch((error) => {
+        alert(`Error: ${error.message}`);
+      });
+  };
+
+
+  return (
+    <div className="loginpageContainer">
+      <form method="POST" onSubmit={handleSubmit} className="logininfoContainer">
+        <p>Email:</p>
+        <input type="email" name="email" onChange={handleChange} />
+        <p>Password:</p>
+        <input type="password" name="password" onChange={handleChange} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login
