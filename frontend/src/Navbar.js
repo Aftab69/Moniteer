7f6d1e6e85a7bf670c43ba00b03c7701037dce34
Aftab69 <aftabhssn3@gmail.com>
2023-04-28 02:35:24 +0530

    navbar changes 2

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const navStyle = { textDecoration:"none", color:"rgb(202, 202, 202)"}
-  const [ loginvisibility, setLoginvisibility ] = useState({visibility:"visible"});
-  const [ logoutvisibility, setLogoutvisibility ] = useState({visibility:"hidden"});
+  const [ loginvisibility, setLoginvisibility ] = useState({display:"block"});
+  const [ logoutvisibility, setLogoutvisibility ] = useState({display:"none"});
 
   const authenticateforaccount = async() =>{
     try{
@@ -21,8 +23,8 @@ const Navbar = () => {
           const data = await res.json();
           // setProfiledata(data);
           // setUserdata(data);
-          setLoginvisibility({visibility:"hidden"})
-          setLogoutvisibility({visibility:"visible"})
+          setLoginvisibility({display:"none"})
+          setLogoutvisibility({display:"block"})
           // console.log(profiledata)
       } else if(res.status===400){
         
@@ -36,6 +38,26 @@ useEffect(()=>{
     authenticateforaccount()
   },[]) // eslint-disable-line react-hooks/exhaustive-deps
 
+  //handling Logout
+  const handleLogout = async(e) =>{
+    e.preventDefault();
+    try{
+      const res = await fetch("https://moniteer-backend.infinityymedia.com/logout",{
+        method:"GET",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        credentials:"include"
+      })
+      if(res.status===200){
+        alert("User Successfully Logged Out !!")
+        navigate("/login")
+      } 
+    }catch(error){
+      console.log(error)
+    }
+  }
+
   return (
     <>
         <nav>
@@ -43,7 +65,7 @@ useEffect(()=>{
             <NavLink style={navStyle} to="/profile"><span>PROFILE</span></NavLink>
             <NavLink style={navStyle} to="/login"><span style={loginvisibility}>LOGIN</span></NavLink>
             <NavLink style={navStyle} to="/register"><span>REGISTER</span></NavLink>
-            <button style={logoutvisibility}>LOG OUT</button>
+            <button onClick={handleLogout} style={logoutvisibility}>LOG OUT</button>
             {/* <NavLink style={navStyle} to="/admin"><span>ADMIN</span></NavLink> */}
         </nav>
     </>
