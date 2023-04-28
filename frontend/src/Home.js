e0b81c7757b0216e8ce1c4f2cc69b0d03d81ed53
Aftab69 <aftabhssn3@gmail.com>
2023-04-28 11:28:15 +0530

    sub admin functionalities added 8

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -193,6 +193,7 @@ const Home = () => {
   }
 
   const handlerole = (e) =>{
+    e.preventDefault();
     const membernm = e.target.name;
     setMemberdata({...memberdata,
       company:companyname,
